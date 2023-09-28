2aa91feb3e49d931bc18b2516f60514ed663fa2b
Vaibhav Srivastava <vaibhavs@netlink.com>
2023-09-28 09:53:07 +0000

    ALiasChanges

--- models/test_models/yearwise_sales.sql
+++ models/test_models/yearwise_sales.sql
@@ -6,7 +6,7 @@ select
     p.product_id,
     p.category,
     p.product,
-    year(o.order_date) as year,
+    year(o.order_date) as yearOnly,
     sum(o.sales) as total_sales
 from orders o
 inner join products p on p.product_id = o.product_id
