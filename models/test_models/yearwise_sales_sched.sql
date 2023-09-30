de22e8321bc4407c5413b05bd3e39d838b063b50
Vaibhav Srivastava <vaibhavs@netlink.com>
2023-09-30 18:46:29 +0000

    Testing pull requests

--- models/test_models/yearwise_sales_sched.sql
+++ models/test_models/yearwise_sales_sched.sql
@@ -1,6 +1,7 @@
 with
     products as (select product_id, category, product from SF_DBT_LANDING.RETAIL_CSV_DATA.products),
     orders as (select product_id, order_date, sales from SF_DBT_LANDING.RETAIL_CSV_DATA.order_demo)
+    
 
 select
     p.product_id,
